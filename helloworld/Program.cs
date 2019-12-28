d378e5cff4fcb8dbfd2e79dcc5f8647fa2daa74d
root <root@localhost.localdomain>
2019-12-29 02:53:42 +0300

    changes test 2

--- helloworld/Program.cs
+++ helloworld/Program.cs
@@ -10,7 +10,7 @@ namespace helloworld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Worldd!");
+            Console.WriteLine("Hello Worlddasdfghj!");
             Console.ReadKey();
         }
     }
