7e52c7819f4ee32004457e10b277259bc564a8a8
mvv101 <mardeevvv@gmail.com>
2019-12-29 02:49:48 +0300

    changes test

--- helloworld/Program.cs
+++ helloworld/Program.cs
@@ -10,7 +10,7 @@ namespace helloworld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Worldd!");
             Console.ReadKey();
         }
     }
